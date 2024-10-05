2d257494ad01f5871634fa362fb199977253029c
CodeMorth <kevinsalinasvd@gmail.com>
2024-10-05 17:49:42 -0500

    Comment added in the metadata function

--- src/components/global/Metadata.tsx
+++ src/components/global/Metadata.tsx
@@ -1,18 +1,27 @@
-"use client"
-import { usePathname } from "next/navigation";
-import { useLanguage } from "@/Hooks";
-import { getMetadata } from "@/utils/getMetadata";
+'use client'
 
-// Componente que maneja la metadata
+// Import necessary hooks and utility functions
+import { usePathname } from 'next/navigation' // Hook to access the current pathname
+import { useLanguage } from '@/Hooks' // Custom hook to manage language settings
+import { getMetadata } from '@/utils/getMetadata' // Function to fetch metadata based on language and path
+
+// Component that handles the metadata for the current page
 export const Metadata = () => {
-    const pathname = usePathname(); // Usa el hook para obtener la ruta actual
-    const { typeLanguage } = useLanguage(); // Usa el hook para obtener el estado del idioma
-    const metadata = getMetadata(typeLanguage,pathname); // Llama a la función para obtener metadata
-  
-    return (
-      <>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </>
-    );
-  };
+  // Get the current pathname using the usePathname hook
+  const pathname = usePathname()
+
+  // Get the current language setting using the useLanguage hook
+  const { typeLanguage } = useLanguage()
+
+  // Call the getMetadata function to retrieve metadata based on the current language and pathname
+  const metadata = getMetadata(typeLanguage, pathname)
+
+  return (
+    <>
+      {/* Set the document title dynamically based on the fetched metadata */}
+      <title>{metadata.title}</title>
+      {/* Set the meta description dynamically based on the fetched metadata */}
+      <meta name="description" content={metadata.description} />
+    </>
+  )
+}
